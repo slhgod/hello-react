f15423c1d46547d5e78d298e7dd1aba0f8016c15
lh.song <lh.song@impix.co.kr>
2022-07-08 13:22:12 +0900

    올라가

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@
 // }
 
 import React, {Component} from "react";
+import IterationSample from "./IterationSample";
 import ScrollBox from "./ScrollBox";
 import ValidationSample from "./ValidationSample";
 
@@ -25,15 +26,16 @@ class App extends Component {
     return (
       // <ValidationSample/>
       // <ScrollBox/>
-      <div>
-        <ScrollBox ref={(ref) => this.ScrollBox =ref}/>
-        <button onClick={()=> this.ScrollBox.scrollToBottom()}>맨 밑으로</button>
-        {/* 컴포넌트가 처음 렌더링 될때는 ScrollBox가 undefined
-        따라서 ScrollBox.scrollToBottom 값을 읽어올 때 오류가 발생
-        화살표 함수 문법으로 아예 새로운 함수 만들어 메서드 실행 시
-        버튼 누르기 전에 이미 한번 렌더링으로 ScrollBox가 실행되었으므로
-        버튼 눌러서 ScrollBox.scrollToBottom 값을 읽어와도 오류 발생 없음*/}
-      </div>
+      <IterationSample/>
+      // <div>
+      //   <ScrollBox ref={(ref) => this.ScrollBox =ref}/>
+      //   <button onClick={()=> this.ScrollBox.scrollToBottom()}>맨 밑으로</button>
+      //   {/* 컴포넌트가 처음 렌더링 될때는 ScrollBox가 undefined
+      //   따라서 ScrollBox.scrollToBottom 값을 읽어올 때 오류가 발생
+      //   화살표 함수 문법으로 아예 새로운 함수 만들어 메서드 실행 시
+      //   버튼 누르기 전에 이미 한번 렌더링으로 ScrollBox가 실행되었으므로
+      //   버튼 눌러서 ScrollBox.scrollToBottom 값을 읽어와도 오류 발생 없음*/}
+      // </div>
     );
   }
 }
